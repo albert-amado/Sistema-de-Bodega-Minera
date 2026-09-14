b493763c2707bd9590dae9c4cf638609844f5e15
Andres <andres.r.b.1502@gmail.com>
2026-09-14 08:25:07 -0500

    fix: minims

---
+++ fix_migrations.py
@@ -0,0 +1,9 @@
+import sqlite3
+conn = sqlite3.connect('db.sqlite3')
+c = conn.cursor()
+c.execute("DELETE FROM django_migrations WHERE app='herramienta'")
+c.execute("DELETE FROM django_migrations WHERE app='almacen'")
+c.execute("DELETE FROM django_migrations WHERE app='prestamo'")
+c.execute("DELETE FROM django_migrations WHERE app='pagina_principal'")
+conn.commit()
+conn.close()
